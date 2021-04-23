bf215cf496fbaf903e44f84f739e36c3577c8bec
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-23 18:19:37 +0100

    fix seeds

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO Users
-VALUE ('duck', 5)
+INSERT INTO Users (name, highscore)
+VALUES ('duck', 5);
 
-INSERT INTO Quizzes
-VALUE ('Entertainment')
+INSERT INTO Quizzes (category)
+VALUES ('Entertainment');
 
-INSERT INTO UserScore
-VALUE (1,1,5)
+INSERT INTO UserScore (user_id, quiz_id, score)
+VALUES (1,1,5);
