4937b2a7afa2574eb930cf1f7dd45d80d4de1c91
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-23 18:06:35 +0100

    add heroku db config

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,5 +1,14 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+let options;
+
+if (process.env.DATABASE_URL) {
+    options = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    }
+} 
+
+const pool = new Pool(options);
 
 module.exports = pool;
